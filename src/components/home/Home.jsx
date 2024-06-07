b17f6bf720778b3359b836bd040907b215f61732
martinasorialanzi <martinasorialanzi@gmail.com>
2024-06-07 20:45:52 -0300

    image domain changed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,54 +1,39 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import Image from "next/image";
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from 'react';
 import Counter from "./Counter";
-import useWindowSize from "../../utils/useWindowSize";
+import useWindowSize from "../../utils/useWindowSize"
 
 const Banner = () => {
-  // const imageRef = useRef(null);
-  // const [isVisible, setIsVisible] = useState(false);
-
-  // useEffect(() => {
-  //   const observer = new IntersectionObserver((entries) => {
-  //     const entry = entries[0];
-  //     setIsVisible(entry.isIntersecting); // Trigger animation based on visibility
-  //   }, { threshold: 0.75 }); // Observe when 25% of the image is in the viewport
-
-  //   if (imageRef.current) {
-  //     observer.observe(imageRef.current);
-  //   }
-
-  //   return () => observer.disconnect(); // Cleanup on unmount
-  // }, [imageRef]);
+  const { width } = useWindowSize();
+  const imageUrl = width < 768? "/wedd.png" : "/wedd-mobile.png";
 
   return (
     <>
-      <section className="grid w-full justify-center items-center">
-        <div className="relative bg-gradient-to-t from-transparent to-[#ffffff]  ">
-          <p className="absolute w-full mt-5 text-[30px] lg:text-[40px] font-medium text-center  ">
-            Nombre & Nombre
+      
+
+      <section className="grid w-full justify-center items-center   ">
+      <div className="relative bg-gradient-to-t from-transparent to-[#ffffff]  ">
+        <p className="absolute w-full mt-5 text-[30px] lg:text-[40px] font-medium text-center  ">Nombre & Nombre</p>
+        
+        <Image
+          src={imageUrl}
+          priority
+          alt='pareja'
+          width={width}
+          height={418}
+          className='justify-self-center'
+        />
+       
+        
+        <section className=" w-full col-span-1 text-center mt-3">
+          <p className="text-[20px] font-normal text-center ">Save the date</p>
+          <p className="text-[15px] font-normal text-center ">
+            2 de Noviembre de 2023
           </p>
-
-          <Image
-            src="/wedd.png"
-            // ref={imageRef}
-            priority
-            alt="pareja"
-            width={window.innerWidth}
-            height={418}
-            className=" justify-self-center"
-          />
-
-          <section className=" w-full col-span-1 text-center mt-3">
-            <p className="text-[20px] font-normal text-center ">
-              Save the date
-            </p>
-            <p className="text-[15px] font-normal text-center ">
-              2 de Noviembre de 2023
-            </p>
-            <Counter />
-          </section>
+         <Counter />
+        </section>
         </div>
       </section>
     </>
