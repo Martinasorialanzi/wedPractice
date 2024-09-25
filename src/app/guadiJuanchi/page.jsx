712653562ab69cf7f2a8f29f3457c3679c7226d0
Victoria <victoriamariateran@gmail.com>
2024-09-25 11:25:25 -0300

    guadi

--- src/app/guadiJuanchi/page.jsx
+++ src/app/guadiJuanchi/page.jsx
@@ -1,31 +1,39 @@
 import Confirmation from "@/components/confirmation/Confirmation";
 import Event from "@/components/event/Event";
 import Banner from "@/components/home/Home";
-import Mas from "@/components/mas/Mas";
 import Regalos from "@/components/regalos/Regalos";
 import Footer from "@/components/footer/Footer";
-import imgSquare1 from "../../public/cuad1.jpeg";
-import imgSquare2 from "/public/cuad2.jpeg";
-import imgSquare3 from "/public/cuad3.jpeg";
-import imgppal from '/public/wedd.png';
-import { Inika } from "next/font/google";
+import imgSquare1 from "./assets/2.webp";
+import imgSquare2 from "./assets/1.webp";
+import imgSquare3 from "./assets/3.webp";
+import imgppal from './assets/1.webp';
+import { Montserrat } from "next/font/google";
+import { Dancing_Script } from "next/font/google";
 
 
-const inika = Inika({ subsets: ["latin"], weight: ["400", "700"] });
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["500", "700"] });
+const dancing = Dancing_Script({ subsets: ["latin"], weight: ["400", "700"] });
 
 
 export default function Home() {
-  const buttonBgColor = "bg-[#5E5E5E]";
-  const buttonHoverColor = "hover:bg-[#4a4a4a]";
-  const buttonTextColor = "text-[#ffffff]";
+  const buttonBgColor = "bg-[#ffffff]";
+  const buttonHoverColor = "hover:bg-[#ffffff]";
+  const buttonTextColor = "text-[#000000]";
   const textColor = "text-[#000000]";
 
   return (
     <>
-    <main className={`flex min-h-screen flex-col items-center justify-between ${inika.className}`}>
+    <main className={`flex min-h-screen flex-col items-center justify-between ${montserrat.className}`}
+    style={{
+        backgroundImage: `url('/guadijuanchy/fondo.png')`,
+        backgroundSize: '100% auto', // Ajusta el ancho al 100% y deja la altura automática
+        backgroundRepeat: 'repeat-y', // Repite solo verticalmente
+        backgroundPosition: 'top', // La posición inicial es arriba
+      
+      }}>
         <Banner
         grid
-        nombres="Nombre & Nombre"
+        nombres="Guadi y Juanchy"
         fechaCountDown="2 de Noviembre"
         img1={imgSquare1}
         img2={imgSquare2}
@@ -35,13 +43,13 @@ export default function Home() {
         textColor={textColor}
         // bannerMobileClass=''
         // sectionClass=""
-        // titleClass=""
+        titleClass="relative mb-5 text-[40px]"
         // relativeClass=""
         // imageClass=""
         // gridContainerClass=""
-        // saveTheDateClass=""
+        saveTheDateClass={`${dancing.className} text-[45px]`}
         // fechaCountDownClass=""
-        // counterSectionClass=""
+        counterSectionClass="mt-7 mb-10"
         // counterClass = ''
         divGrid="max-w-4xl "
         divGridImage="w-1/3"
@@ -49,15 +57,16 @@ export default function Home() {
       <Event
       extra={false}
         ceremonia
-        lugarCeremonia="Iglesia Nuestra Señora del Valle"
-        linkCeremonia="https://www.google.com/maps/place/Parroquia+Nuestra+Se%C3%B1ora+del+Valle/@-26.815749,-65.293438,16z/data=!4m6!3m5!1s0x942242d62123af13:0xaa2edb3420b1b358!8m2!3d-26.815749!4d-65.293438!16s%2Fg%2F1tfwlfys?hl=es&entry=ttu"
-        horarioCeremonia="17 hs"
+        lugarCeremonia="Iglesia San Cristobal"
+        linkCeremonia="https://www.google.com.ar/maps/place/Capilla+San+Cristobal/@-26.8066524,-65.2964194,17z/data=!3m2!4b1!5s0x942242d1b9cf26eb:0x7d35d64ee884c359!4m6!3m5!1s0x942242d1b9586f13:0x7ba4d2ff3c5037ee!8m2!3d-26.8066572!4d-65.2938445!16s%2Fg%2F11byp62dky?entry=ttu&g_ep=EgoyMDI0MDkyMy4wIKXMDSoASAFQAw%3D%3D"
+        horarioCeremonia="18 hs"
         fiesta
-        lugarFiesta="San Pablo Country Club"
-        linkFiesta="https://www.google.com/maps/place/Sal%C3%B3n+Country+San+Pablo/@-26.8472055,-65.3262783,17z/data=!3m1!4b1!4m6!3m5!1s0x942243724e3770d3:0x9bd5dce35e9f0452!8m2!3d-26.8472055!4d-65.323698!16s%2Fg%2F11csqg3qvx?hl=es&entry=ttu"
+        lugarFiesta="Las Marías"
+        linkFiesta="https://www.google.com.ar/maps/place/Sal%C3%B3n+de+Fiestas+Las+Marias/@-26.8639321,-65.3470091,17z/data=!3m1!4b1!4m6!3m5!1s0x94224385fd17d321:0xa6df0da440dca6fe!8m2!3d-26.8639369!4d-65.3444342!16s%2Fg%2F11cm9lxlgc?entry=ttu&g_ep=EgoyMDI0MDkyMy4wIKXMDSoASAFQAw%3D%3D"
         horarioFiesta="19hs"
         buttonClassColors={`${buttonBgColor} ${buttonTextColor} ${buttonHoverColor}`}
         textColor={textColor}
+        
         // sectionClass=""
         // titleClass=""
         // textClass=""
@@ -70,7 +79,7 @@ export default function Home() {
 
       />
        <Confirmation
-        linkConfirmacion="https://docs.google.com/forms/d/e/1FAIpQLScZvrbe-s09VXlI9R-CqH7trlfLHaH_2AgQVfWuelOCFCNOSg/viewform"
+        linkConfirmacion="https://docs.google.com/forms/d/1_YOJ2NE_SieAiHAcNOK7Hq_DiiaWOiWa9lP793UBoYQ/viewform"
         buttonClassColors={`${buttonBgColor} ${buttonTextColor} ${buttonHoverColor}`}
         textColor={textColor}
         descripcion='Nuestro festejo no sería lo mismo sin vos. Confirmanos tu presencia a
@@ -86,33 +95,19 @@ export default function Home() {
         alias="Mi Alias"
         img1="https://i.ibb.co/pb4kXxr/adversal.webp"
         img2="https://i.ibb.co/9pHQws1/Vector.webp"
-        regaloLista={true}
+        regaloLista={false}
         regaloAlias={true}
         buttonClassColors={`${buttonBgColor} ${buttonTextColor} ${buttonHoverColor}`}
         textColor={textColor}
         // buttonClass=""
-        // sectionClass=""
+        sectionClass="w-[100%] justify-center items-center text-center my-1 lg:px-[25px] mb-20"
         // imageClass=""
         // titleClass=""
         // descriptionClass=""
         // modalBackgroundClass=""
         // modalContentClass=""
       />
-      <Mas
-        dresscode="Elegante"
-        spotify="https://open.spotify.com/playlist/37i9dQZEVXbMMy2roB9myp?si=_sxc0d_kTNWW32BpyQadcg&pi=u-TONuDD0FSg-u"
-        fotos="https://drive.google.com/drive/folders/1NNdkgI-3xKHxj6R2vnVzJo7nKox-GSr5?usp=drive_link"   
-        buttonClassColors={`${buttonBgColor} ${buttonTextColor} ${buttonHoverColor}`}
-        textColor={textColor}   
-        // sectionClass=""
-        // imageClass=""
-        // titleClass=''
-        // descriptionClass=""  
-        // buttonClass=""
-        // modalBackgroundClass=""
-        // modalContentClass=""
-      />
-      
+   
     </main>
     <Footer footerClassName={`${buttonBgColor} h-[57px]`}/>
     </>
