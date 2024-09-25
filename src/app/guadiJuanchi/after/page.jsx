2fc15b5b71e7400c4d5ee4f5bb563dcc64db4ed6
Victoria <victoriamariateran@gmail.com>
2024-09-25 18:00:50 -0300

    despues de 12

--- src/app/guadiJuanchi/after/page.jsx
+++ src/app/guadiJuanchi/after/page.jsx
@@ -3,15 +3,11 @@ import Event from "@/components/event/Event";
 import Banner from "@/components/home/Home";
 import Regalos from "@/components/regalos/Regalos";
 import Footer from "@/components/footer/Footer";
-import imgSquare1 from "./assets/2.webp";
-import imgSquare2 from "./assets/1.webp";
-import imgSquare3 from "./assets/3.webp";
-import imgppal from './assets/1.webp';
+import imgSquare1 from "../assets/2.webp";
+import imgSquare2 from "../assets/1.webp";
+import imgSquare3 from "../assets/3.webp";
+import imgppal from '../assets/1.webp';
 import { Montserrat } from "next/font/google";
-import { Dancing_Script } from "next/font/google";
-import { Great_Vibes } from "next/font/google";
-import { Allura } from "next/font/google";
-import { Satisfy } from "next/font/google";
 import { Parisienne } from "next/font/google";
 
 
@@ -67,7 +63,7 @@ export default function Home() {
         fiesta
         lugarFiesta="Las Marías"
         linkFiesta="https://www.google.com.ar/maps/place/Sal%C3%B3n+de+Fiestas+Las+Marias/@-26.8639321,-65.3470091,17z/data=!3m1!4b1!4m6!3m5!1s0x94224385fd17d321:0xa6df0da440dca6fe!8m2!3d-26.8639369!4d-65.3444342!16s%2Fg%2F11cm9lxlgc?entry=ttu&g_ep=EgoyMDI0MDkyMy4wIKXMDSoASAFQAw%3D%3D"
-        horarioFiesta="19hs"
+        horarioFiesta="22:30 hs"
         buttonClassColors={`${buttonBgColor} ${buttonTextColor} ${buttonHoverColor}`}
         textColor={textColor}
         
@@ -96,8 +92,8 @@ export default function Home() {
         
       />
        <Regalos
-        alias='juanchiyguadi '
-        cbu='0150518401000161269259 Banco ICBC'
+        alias='juanchiyguadi Banco ICBC'
+        cbu='0150518401000161269259 '
         img1="https://i.ibb.co/pb4kXxr/adversal.webp"
         img2="https://i.ibb.co/9pHQws1/Vector.webp"
         regaloLista={false}
