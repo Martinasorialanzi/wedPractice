33cf3c9cac66f1edac51bf9f8750474ee157dc20
Victoria <victoriamariateran@gmail.com>
2024-10-17 11:21:06 -0300

    agrego datos regalo

--- src/app/noemiJorge/page.jsx
+++ src/app/noemiJorge/page.jsx
@@ -93,7 +93,7 @@ export default function NoemiJorge() {
       />
       <Regalos
         iconColor={iconColor}
-        cbu="4530000800014383359592"
+        cbu="4530000800014383359592 a nombre de Jorge Frias, Banco Galicia"
         img1="https://i.ibb.co/pb4kXxr/adversal.webp"
         img2="https://i.ibb.co/9pHQws1/Vector.webp"
         regaloLista={false}
@@ -106,7 +106,7 @@ export default function NoemiJorge() {
         descriptionClass={`lg:text-[23px] font-normal m-4 text-[20px] ${textColor}`}
         // buttonClass=""
         // modalBackgroundClass=""
-        modalContentClass={`bg-white rounded-lg p-6 z-10 lg:p-10 lg:text-[23px] font-normal m-4 text-[23px] ${textColor}`}
+        modalContentClass={`bg-white rounded-lg p-6 z-10 lg:p-10 text-[23px] lg:text-[23px] font-normal m-4  w-[59%] ${textColor}`}
       />
       <Mas
         iconColor={iconColor}
